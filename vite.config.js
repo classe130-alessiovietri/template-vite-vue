3731f52e163777b7ca6a7c85f4f74c464e0cfda0
Alessio Vietri <info@alessiovietri.it>
2024-09-12 10:43:40 +0200

    Aggiunto anche additionalData per scss

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          // @use 'partials/reset' as *;
+          @use 'partials/variables' as *;
+          @use 'partials/mixins' as *;
+        `
+      }
+    }
+  }
 })
